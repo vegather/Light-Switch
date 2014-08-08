e2a24617d445826eff5ae8abb490c05e07ae7462
Vegard <vegather@gmail.com>
2014-08-09 00:05:36 +0200

    Initial Commit

---
+++ Dimmer 2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Dimmer 2
+//
+//  Created by Vegard Solheim Theriault on 09/08/14.
+//  Copyright (c) 2014 Vegard Solheim Theriault. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Dimmer 2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dimmer 2
+//
+//  Created by Vegard Solheim Theriault on 09/08/14.
+//  Copyright (c) 2014 Vegard Solheim Theriault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
